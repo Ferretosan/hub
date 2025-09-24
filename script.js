6d870c290a8835a9f4c9b4a167a1ba5eb1e08379
Ferret <129646624+ferretosan@users.noreply.github.com>
2025-09-24 19:07:50 +1000

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+// Navigation functions
+function navigateToAstragames() {
+    window.open('https://astragames.ferretosan.com', '_blank');
+}
+
+function navigateToMainWebsite() {
+    window.open('https://ferretosan.com', '_blank');
+}
+
+function showComingSoon(gameName) {
+    alert(`${gameName} is coming soon! Stay tuned for updates.`);
+}
